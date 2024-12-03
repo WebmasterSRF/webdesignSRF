9d9834bf1647869e5a1dfbe5bec091c49f5ccbe3
WebmasterSRF <webmaster@rs-feucht.de>
2024-12-03 14:30:53 +0100

    Umstellen auf PHP

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
+<?php 
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,5 +11,13 @@
 <body>
     Test
     Hallo, das ist der Test von Paul
+
+    <?php 
+    $var1 = 4;
+    $var2 = 4;
+    $var3 = $var1 + $var2;
+ echo "Dies ist über PHP gedruckt" . $var3; ?>
+
+
 </body>
 </html>
